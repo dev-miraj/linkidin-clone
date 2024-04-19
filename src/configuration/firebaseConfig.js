fe00f85a72bd958e338e00667d5abb631537263c
Md Mirajul Islam <144055650+miraj-2302@users.noreply.github.com>
2024-04-19 05:32:49 +0530

    firebaseConfig.js

--- src/configuration/firebaseConfig.js
+++ src/configuration/firebaseConfig.js
@@ -1,18 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCnuJVtKPGx5qichpS6zT9y_L6k2DJ1nEo",
-  authDomain: "linkidin-clone-cc1aa.firebaseapp.com",
-  databaseURL: "https://linkidin-clone-cc1aa-default-rtdb.firebaseio.com",
-  projectId: "linkidin-clone-cc1aa",
-  storageBucket: "linkidin-clone-cc1aa.appspot.com",
-  messagingSenderId: "513676067063",
-  appId: "1:513676067063:web:3e4ba9f5c510b0303ad3f2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
